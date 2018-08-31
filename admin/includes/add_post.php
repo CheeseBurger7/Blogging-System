17006f25424baf717dc2e6a6cbf5883d8f3cda6f
CheeseBurger7 <37046535+CheeseBurger7@users.noreply.github.com>
2018-08-31 16:26:35 +0530

    Update add_post.php

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,1 +1,50 @@
-hello
+<?php
+	if(isset($_POST['create_post'])){
+		echo $_POST['title'];
+	}
+
+
+?>
+
+
+<form action="" method="post" enctype="multipart/form-data"> 
+	<div class="form-group">
+		<label for="title">Post Title</label>
+		<input type="text" name="title" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="post_category">Post Category</label>
+		<input type="text" class="form-control" name="post_category_id">
+	</div>
+
+	<div class="form-group">
+		<label for="author">Post Author</label>
+		<input type="text" name="author" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="post_status">Post Status</label>
+		<input type="text" name="post_status" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="post_image">Post Image</label>
+		<input type="file" name="image">
+	</div>
+
+	<div class="form-group">
+		<label for="post_tags">Post Tags</label>
+		<input type="text" name="post_tags" class="form-control">
+	</div>
+	
+	<div class="form-group">
+		<label for="post_content">Post Content</label>
+		<textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea> 
+	</div>
+
+	<div class="form-group">
+		<input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+	</div>
+
+</form>
